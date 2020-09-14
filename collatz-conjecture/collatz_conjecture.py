c6e8cad7a4e6e51a21b4f63d69954a4c4b5a66ea
bcahn2 <byeongchan.an@chicagobooth.edu>
2020-09-14 19:21:43 +0000

    start working on conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,20 @@
 def steps(number):
-    pass
+    n = 0
+    number_ini = number
+    print(n, number)
+ 
+    while (number != 1):
+        if number % 2 == 0:
+            number = int(number / 2)
+            n = n + 1
+            print(n, number)
+        else:
+            number = int(number*3 + 1)
+            n = n + 1
+            print(n, number)
+
+    return("For input n = " + str(number_ini) + ", the return value would be " + str(n) )
+    
+    
+steps(12)
+
