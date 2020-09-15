2e02522ff6e4715863c84015717744a0c92518c1
zijun-rp <zijun.xu@chicagobooth.edu>
2020-09-15 15:33:23 +0000

    luhn changed

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,7 +1,21 @@
+def double(number):
+    doubled = 2*number
+    if doubled > 9:
+        return doubled - 9
+    else:
+        return doubled
+
 class Luhn(object):
     def __init__(self, card_num):
-        self_cleaned = card_num.replace(" ", "")
-        self_doubled = [2*int(x) for ind, x in enumerate(self_cleaned) if (ind%2)==0]
+        # Reverse the list and remove whitespaces
+        self.cleaned = list(reversed([int(s) for s in card_num if s.isdigit()]))
+
+        # Double
+        self.doubled = [double(num) for ind, num in enumerate(self.cleaned) if (ind%2)==0]
 
     def valid(self):
-        pass
+        # Check if the sum if divisible by 10
+        if sum(self.doubled) % 10 == 0:
+            print("Valid")
+        else:
+            print("Invalid")
