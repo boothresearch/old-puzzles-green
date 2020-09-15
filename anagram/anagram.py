85e050293fc1a3471855b6cc7118ca4c47c566fd
jaymokimbooth <jaymo.kim@chicagobooth.edu>
2020-09-15 15:29:39 +0000

    anagram finished, test passed

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,7 @@
 def find_anagrams(word, candidates):
-    pass
+    result = []
+    word_sorted = sorted(word.lower())
+    for i in range(len(candidates)):
+        if word.lower() != candidates[i].lower() and word_sorted == sorted(candidates[i].lower()):
+            result.append(candidates[i])
+    return result
