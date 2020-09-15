3b5a2f243a04abf77c7e07c4f5eedac6a0930548
jiguangl <jiguang.li@chicagobooth.edu>
2020-09-15 15:25:59 +0000

    finished problem 9

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,23 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    sentence=sentence.lower()
+    sentence= " ".join(sentence.split())
+    sentence = re.split("[^a-zA-Z0-9|'t|'s|'ve]", sentence)
+    sentence=[sentence[i].strip() for i in range(len(sentence))]
+    result={}
+    for i in range(len(sentence)):
+        if sentence[i]!="" :
+            if sentence[i][0]=="'" and sentence[i][-1]=="'":
+                sentence[i]= sentence[i].replace("'", "")
+            if sentence[i] in result:
+                result[sentence[i]]+=1
+            else:
+                result[sentence[i]]=1
+
+    return result 
+
+
+#count_words("one fish two fish red fish blue fish")
+
